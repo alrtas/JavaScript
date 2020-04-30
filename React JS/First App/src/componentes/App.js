2dcd2e1d26b540854c3f659d376d05a1c8fb0e4f
Thiago <alrrtas@gmail.com>
2020-04-29 23:37:24 -0300

    my first webApp

---
+++ React JS/First App/src/componentes/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Post from './Post';
+
+export default class App extends React.Component{
+    render(){
+        return(
+        <div>
+            <h1>Hello World</h1>
+            <Post title="Aprendendo ReactJS"/>
+            <Post title="ReactJS é demais!"/>
+            <Post title="Em breve mais reactJS"/>
+        </div>
+        );
+    }
+}
