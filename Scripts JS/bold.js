d24c9a2d780870d2959ac501e7c99f76f415517b
Thiago Alberto da Silva <32065208+alrtas@users.noreply.github.com>
2020-05-18 15:02:11 -0300

    Creat bold.js

---
+++ Scripts JS/bold.js
@@ -0,0 +1,29 @@
+            /**
+            * Esse script deve estar no parametros de entrada do fluxo, em ações globais.
+            * para acessar em qualquer lugar do blip, basta usar  {{n@i}} no inicio e {{n@f}} no final
+            **/
+            function run() {
+                var o = '{{contact.source}}';
+
+                if (o=='0mn.io'){
+                    return obj = { 
+                        'i': '<b>',
+                        'f': '</b>'
+                    }
+                }
+                else if (o=='WhatsApp'){
+                    return obj = {
+                        'i':'*',
+                        'f':'*'
+                    }
+                }
+                else if (o=="Telegram"){
+                    return ''
+                }
+                else if (o=='Messenger'){
+                    return ''
+                }
+                else{
+                    return ''
+                }
+            }
